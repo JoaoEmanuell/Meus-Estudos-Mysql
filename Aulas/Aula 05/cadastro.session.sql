3ecf8bc62018d95e2efba7d12c4435e027da3f03
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2021-05-06 15:40:08 -0300

    aula 05 full

---
+++ Aulas/Aula 05/cadastro.session.sql
@@ -0,0 +1,7 @@
+
+insert into pessoas values
+(DEFAULT, 'Mario', '1999-07-20', 'M', '60.5', '1.60', DEFAULT),
+(DEFAULT, 'Joana', '1997-08-31', 'F', '70.5', '1.63', 'EUA'),
+(DEFAULT, 'Nino', '1980-01-25', 'M', '80.0', '2.0', DEFAULT);
+
+select * from pessoas
